61685a56bec5c1da9fee47e0ed6cc1a3db37f0f5
ptt <25803050339@m.fudan.edu.cn>
2026-03-28 11:35:00 +0800

    add lab color space code

---
+++ utils/__init__.py
@@ -0,0 +1,19 @@
+from .lab_utils import (
+    rgb_to_lab,
+    lab_to_rgb,
+    normalize_lab,
+    denormalize_lab,
+    split_l_ab,
+    merge_l_ab,
+    prepare_input_target,
+)
+
+__all__ = [
+    "rgb_to_lab",
+    "lab_to_rgb",
+    "normalize_lab",
+    "denormalize_lab",
+    "split_l_ab",
+    "merge_l_ab",
+    "prepare_input_target",
+]
